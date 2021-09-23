f045a38add0229846d160fe3158fc6b46f6f8890
seancary62 <sean.cary62@gmail.com>
2021-09-23 19:25:37 -0400

    Added geoJSON markers and majorAirports.json

--- Simple_Map/static/js/logic.js
+++ Simple_Map/static/js/logic.js
@@ -2,7 +2,7 @@
 console.log("working");
 
 // Create the map object with a center and zoom level.
-let map = L.map('mapid').setView([37.6213, -122.3790], 5);
+let map = L.map('mapid').setView([37.5, -122.5], 10);
 
 
 
@@ -10,7 +10,7 @@ let map = L.map('mapid').setView([37.6213, -122.3790], 5);
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
-    id:  "light-v10",
+    id:  "streets-v11",
     accessToken: API_KEY
 });
 // Then we add our 'graymap' tile layer to the map.
@@ -19,29 +19,36 @@ streets.addTo(map);
 // Get data from cities.js
 let cityData = cities;
 
-// Loop through the cities array and create one marker for each city.
-cityData.forEach(function(city) {
-    console.log(city)
-    L.circleMarker(city.location, {
-        radius: city.population/100000
-    })
-    .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
-    .addTo(map);
-   });
+// Add GeoJSON data.
+let sanFranAirport =
+{"type":"FeatureCollection","features":[{
+    "type":"Feature",
+    "properties":{
+        "id":"3469",
+        "name":"San Francisco International Airport",
+        "city":"San Francisco",
+        "country":"United States",
+        "faa":"SFO",
+        "icao":"KSFO",
+        "alt":"13",
+        "tz-offset":"-8",
+        "dst":"A",
+        "tz":"America/Los_Angeles"},
+        "geometry":{
+            "type":"Point",
+            "coordinates":[-122.375,37.61899948120117]}}
+]};
 
-// Coordinates for each point to be used in the line.
-let line = [
-    [37.6213, -122.3790],
-    [30.1975, -97.6664],
-    [43.6777, -79.6248],
-    [40.6413, -73.7781]
-  ];
 
-// Create a polyline using the line coordinates and make the line red.
-L.polyline(line, {
-    color: "blue",
-    weight: '4',
-    dashArray: '5, 5', 
-    opacity: 0.5
+// Grabbing our GeoJSON data.
+L.geoJSON(sanFranAirport).addTo(map);
+
+// Grabbing our GeoJSON data.
+L.geoJson(sanFranAirport, {
+    // We turn each feature into a marker on the map.
+    onEachFeature: function(feature, layer) {
+      console.log(feature);
+      layer.bindPopup('<h2>'+feature.properties.name+'</h2>' + '<br>' + '<h3>' + feature.properties.city+ ", " + feature.properties.country + '</h3>');
+    }
   }).addTo(map);
-  
+
