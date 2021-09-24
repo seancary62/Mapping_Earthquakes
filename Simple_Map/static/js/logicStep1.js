3eabcef946d44bd226df9e576b667c7ce040529c
seancary62 <sean.cary62@gmail.com>
2021-09-24 12:18:24 -0400

    changing map markers to circles that change size based on magnitude

--- Simple_Map/static/js/logicStep1.js
+++ Simple_Map/static/js/logicStep1.js
@@ -1,4 +1,7 @@
 
+// Add console.log to check to see if our code is working.
+console.log('working');
+
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
@@ -16,13 +19,6 @@ let satelliteStreets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/{id}
     accessToken: API_KEY
 });
 
-// let light = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/navigation-guidance-day-v4/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-//     attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
-//     maxZoom: 18,
-//     accessToken: API_KEY
-// });
-
-
 // Create a base layer that holds both maps.
 let baseMaps = {
     'Street': streets,
@@ -31,43 +27,49 @@ let baseMaps = {
 
 // Create the map object with center, zoom level and default layer.
 let map = L.map('mapid', {
-    center: [43.7, -79.3],
-    zoom: 11,
+    center: [39.5, -98.5],
+    zoom: 3,
     layers: [streets]
 });
 
 // Pass our map layers into our layers control and add the layers control to the map.
 L.control.layers(baseMaps).addTo(map);
 
-// Accessing the Toronto airline routes GeoJSON URL.
-let torontoHoods = "https://raw.githubusercontent.com/seancary62/Mapping_Earthquakes/Mapping_GeoJSON_Polygons/Simple_Map/torontoNeighborhoods.json";
-
-
-// Then we add our 'graymap' tile layer to the map.
-streets.addTo(map);
-
-// Create a style for the lines.
-let myStyle = {
-    color: "blue",
-    weight: 1,
-    fillColor: 'yellow',
-};
-
-  // Grabbing our GeoJSON data.
-d3.json(torontoHoods).then(function(data) {
-    console.log(data);
+// Retrieve the earthquake GeoJSON data.
+d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(function(data) {
   // Creating a GeoJSON layer with the retrieved data.
   L.geoJson(data).addTo(map);
 });
 
+// // Accessing the Toronto airline routes GeoJSON URL.
+// let torontoHoods = "https://raw.githubusercontent.com/seancary62/Mapping_Earthquakes/Mapping_GeoJSON_Polygons/Simple_Map/torontoNeighborhoods.json";
 
-d3.json(torontoHoods).then(function(data) {
-    L.geoJson(data, {
-        style: myStyle,
-        // We turn each feature into a marker on the map.
-        onEachFeature: function(feature, layer) {
-        console.log(feature);
 
-        layer.bindPopup('<h3>'+ "Neighborhood: "+ feature.properties.AREA_NAME +'</h3>')}
-    }).addTo(map);
-});
+// // Then we add our 'graymap' tile layer to the map.
+// streets.addTo(map);
+
+// // Create a style for the lines.
+// let myStyle = {
+//     color: "blue",
+//     weight: 1,
+//     fillColor: 'yellow',
+// };
+
+//   // Grabbing our GeoJSON data.
+// d3.json(torontoHoods).then(function(data) {
+//     console.log(data);
+//   // Creating a GeoJSON layer with the retrieved data.
+//   L.geoJson(data).addTo(map);
+// });
+
+
+// d3.json(torontoHoods).then(function(data) {
+//     L.geoJson(data, {
+//         style: myStyle,
+//         // We turn each feature into a marker on the map.
+//         onEachFeature: function(feature, layer) {
+//         console.log(feature);
+
+//         layer.bindPopup('<h3>'+ "Neighborhood: "+ feature.properties.AREA_NAME +'</h3>')}
+//     }).addTo(map);
+// });
